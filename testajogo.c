90b546df186903a40453f0baa01d1323c6772bf9
Ícaro Harry <icaropc17@gmail.com>
2014-04-24 20:11:54 -0300

    Create testajogo.c

---
+++ testajogo.c
@@ -0,0 +1,29 @@
+#include "guerreiro.h"
+
+int main(){
+    guerreiro g1, g2;
+    int vencedor;
+
+    criaGuerreiro(&g1);
+    criaGuerreiro(&g2);
+
+    while(1){
+        ataca(&g1, &g2);
+        if(g2.pontos_de_vida>0){
+            ataca(&g2, &g1);
+            if(g1.pontos_de_vida <= 0){
+                vencedor = g2.id_jogador;
+                break;
+            }
+
+        }
+        else{
+            vencedor = g1.id_jogador;
+            break;
+        }
+    }
+    printf("Vencedor: %d\n", vencedor);
+    return 0;
+}
+
+
